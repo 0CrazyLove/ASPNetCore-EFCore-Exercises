2b04132edc550b094a257f1f8cda9e6454b4ae7e
Crazy <axelvasquez582@gmail.com>
2025-07-01 00:05:56 -0500

    ...

--- Ejercicio1/Ejercicio1/Models/Producto.cs
+++ Ejercicio1/Ejercicio1/Models/Producto.cs
@@ -1,11 +1,16 @@
-﻿namespace Ejercicio1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio1.Models
 {
     public class Producto
     {
         //properties
         public int? Id { get; set; }
+        [Required]
         public string? Nombre { get; set; }
+        [Required]
         public decimal? Precio { get; set; }
+        [Required]
         public int? Stock { get; set; }
 
     }
