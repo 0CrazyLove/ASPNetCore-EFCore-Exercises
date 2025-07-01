7b08dcb18e9bfcea216ec7167161caa46329fd9b
Crazy <axelvasquez582@gmail.com>
2025-06-30 23:44:54 -0500

    ...

---
+++ Ejercicio1/Ejercicio1/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿
+using Ejercicio1.Data;
+using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Ejercicio1.Controllers;
+public class ProductoController : Controller
+{
+    private readonly AppDbContext _context;
+    public ProductoController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    // GET: Producto/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+    // POST: Producto/Create
+    [HttpPost]
+    public IActionResult Create(Producto producto)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Productos.Add(producto);
+            _context.SaveChanges();
+            return RedirectToAction("Crear");
+        }
+        return View(producto);
+    }
+
+}
+
--- Ejercicio1/Ejercicio1/Models/Producto.cs
+++ Ejercicio1/Ejercicio1/Models/Producto.cs
@@ -3,10 +3,10 @@
     public class Producto
     {
         //properties
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Nombre { get; set; }
-        public decimal Precio { get; set; }
-        public int Stock { get; set; }
+        public decimal? Precio { get; set; }
+        public int? Stock { get; set; }
 
     }
 }
