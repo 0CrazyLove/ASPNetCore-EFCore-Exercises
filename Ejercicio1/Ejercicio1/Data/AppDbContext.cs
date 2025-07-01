5190317dd5ae9f96cf2929c2a7353198c03fc266
Crazy <axelvasquez582@gmail.com>
2025-06-30 23:07:49 -0500

    ...

---
+++ Ejercicio1/Ejercicio1/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ejercicio1.Models;
+namespace Ejercicio1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Producto> Productos { get; set; }
+
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio1.Models
+{
+    public class Producto
+    {
+        //properties
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
