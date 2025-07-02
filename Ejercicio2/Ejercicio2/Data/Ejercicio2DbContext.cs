5f05f17965378ea08eeac5fbb45a5a16ff6ca9ae
Crazy <axelvasquez582@gmail.com>
2025-07-02 17:53:40 -0500

    ...

---
+++ Ejercicio2/Ejercicio2/Data/Ejercicio2DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ejercicio2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Ejercicio2.Data;
+
+public class Ejercicio2DbContext : DbContext
+{
+    public Ejercicio2DbContext(DbContextOptions<Ejercicio2DbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Person> Persons { get; set; } = null!;
+    
+}
+
