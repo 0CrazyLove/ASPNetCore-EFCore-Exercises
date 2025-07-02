d53f3cf376201459c6c5768ddab401ebceb88afb
Crazy <axelvasquez582@gmail.com>
2025-07-02 17:25:59 -0500

    ...

--- Ejercicio1/Ejercicio1/Controllers/ProductoController.cs
+++ Ejercicio1/Ejercicio1/Controllers/ProductoController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ejercicio1.Controllers;
 public class ProductoController : Controller
 {
+    
     private readonly AppDbContext _context;
     public ProductoController(AppDbContext context)
     {
