2ef36c364c0b6f1da1a7c28814b3dcc2bd6d8ce4
Crazy <axelvasquez582@gmail.com>
2025-07-26 22:33:40 -0500

    ...

--- Ejercicio6/Ejercicio6/Ejercicio6/Controllers/UserController.cs
+++ Ejercicio6/Ejercicio6/Ejercicio6/Controllers/UserController.cs
@@ -18,13 +18,22 @@ namespace Ejercicio6.Controllers
         {
             return _context.Users.ToList();
         }
+        [HttpGet("{id}")]
+        public ActionResult<User> SearchUserId(int id)
+        {
+            var User = _context.Users.SingleOrDefault(u => u.Id == id);
+            if (User == null) return NotFound();
+            return Ok(User);
+        }
+
+
         [HttpPost]
         // POST api/user
         public ActionResult<User> CreateUser(User user)
         {
             _context.Users.Add(user);
             _context.SaveChanges();
-            return CreatedAtAction(nameof(GetUsers), new { id = user.Id });
+            return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
         }   
 
     }
